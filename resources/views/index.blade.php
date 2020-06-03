638b1e5585851cda9ac32a7127329dd0cbefa049
henmud <smorcstandin@gmail.com>
2020-06-03 14:37:32 +0800

    zamena href

---
+++ resources/views/index.blade.php
@@ -0,0 +1,56 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GeekStore</title>
+	<link rel="stylesheet" href="css/Style.css">
+	<script src="https://kit.fontawesome.com/ca0fae56c5.js"> </script>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap&subset=cyrillic" rel="stylesheet">
+</head>
+<body>
+	<!--баннер-->
+	<div class="main"> 
+		<div class="banner"></div>
+		<div class="banner-tr"></div>
+		<div class="pic"> 
+			GEEK STORE
+		</div>
+		</div>
+<!--поисковик-->
+<div class = "search-box">
+    <input class="search-txt" type="text" name="" placeholder="search"> 
+    <a href="#" class="search-bth">
+    <i class="fas fa-search" aria-hidden="true"> </i>
+    </a>    		
+</div>
+
+
+	<!--inform-link-->
+	<div class="geek">GEEK</div>
+	<div class="store">STORE</div>
+	<div class="manga">Манга, комиксы, фигурки</div>
+
+	<div class="inform-link">
+		
+		<div class="pos"><a href="/all" class="inform-btn read">Посмотреть</a></div>
+		<div class="kup"><a href="/all" class="inform-btn shop">Купить сейчас</a></div>
+		
+	</div>
+
+
+
+
+<!--Входи,корзина-->
+<div class="personal-item">
+		<div class="auth">
+				<div class="login">
+					<a href="#" class="auth-link"><i class="fas fa-user-circle"></i>Войти</a>
+				</div>
+				<div class="car">
+					<a href="#" class="auth-link"><i class="fas fa-shopping-cart"></i>Корзина</a>
+				</div>
+		</div>
+</div> 
+
+
+</body>
+</html>

d6dfb8f87d2fce165b0a7cdd27da018eb6d7839b
henmud <smorcstandin@gmail.com>
2020-06-03 15:11:43 +0800

    controller

---
+++ resources/views/all.blade.php
@@ -0,0 +1,65 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<title>GeekStore</title>
+	<link rel="stylesheet" href="css/all.css">
+	<script src="https://kit.fontawesome.com/ca0fae56c5.js"> </script>
+	<link href="https://fonts.googleapis.com/css?family=Roboto:400,700&display=swap&subset=cyrillic" rel="stylesheet">
+</head>
+<body>
+
+	<!--баннер-->
+	<div class="main">
+		<div class="banner"></div>
+		<div class="pic">
+			GEEK STORE
+	</div>
+	</div>
+
+<!--поисковик-->
+<div class = "search-box">
+    <input class="search-txt" type="text" name="" placeholder="search"> 
+    <a href="#" class="search-bth">
+    <i class="fas fa-search" aria-hidden="true"> </i>
+    </a>    		
+</div>
+
+<!--манга-->
+
+	<div class="manga1">
+		 <div class="tkg1">
+             <a href="tkgtom1.html" class="tkg1">
+             <img src="pic/ghoul.jpg" alt="Токийский гуль. Том 1." width='150px'  height='200px'>
+              Манга "Токийский гуль" Том 1 купить за 5$ </a>
+         </div>
+    </div>
+        
+<div class="manga3">
+		 <div class="tkg3">
+             <a href="tkgtom2.html" class="tkg1">
+             <img src="pic/ghoul2.jpg" alt="Токийский гуль. Том 2." width='150px'  height='200px'>
+              Манга "Токийский гуль" Том 2 купить за 5$ </a>
+         </div>
+    </div>
+ 
+<div class="manga2">
+		 <div class="tkg2">
+             <a href="tkgtom3.html" class="tkg1">
+             <img src="pic/ghoul3.jpg" alt="Токийский гуль. Том 3." width='150px'  height='200px'>
+              Манга "Токийский гуль" Том 3 купить за 5$ </a>
+         </div>
+    </div>
+        
+
+
+
+
+
+
+
+
+
+
+
+</body>
+</html>
